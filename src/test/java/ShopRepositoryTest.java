cb510c2d3468c2a2958a1ee8c1853fd6e1fe8d21
Irina <kalashiri-sweet@mail.ru>
2023-04-05 00:27:31 +0500

    Added src

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ShopRepositoryTest {
+    @Test
+    public void testWhenRemovedProductExist() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Книга", 500);
+        Product product2 = new Product(2, "Телефон", 20_000);
+        Product product3 = new Product(3, "Кружка", 100);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        repo.remove(2);
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testWhenRemovedProductNotExist() {
+        ShopRepository repo = new ShopRepository();
+        Product product1 = new Product(1, "Книга", 500);
+        Product product2 = new Product(2, "Телефон", 20_000);
+        Product product3 = new Product(3, "Кружка", 100);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.remove(4)
+        );
+
+
+
+    }
+
+}
